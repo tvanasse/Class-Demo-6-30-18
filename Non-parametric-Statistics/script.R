cdb228e617902aae88ebe13c538bed0de2e39cf5
Tom Vanasse <thomas.j.vanasse@gmail.com>
2018-05-31 18:38:05 -0500

    commit 5-31-18

--- Non-parametric-Statistics/script.R
+++ Non-parametric-Statistics/script.R
@@ -1,5 +1,13 @@
 #Non-parametric Statistics Lecture(6/1/18)
 #Tom Vanasse 
+library(ggplot2)
+data("cars")
+
+#Spearman Rank Correlation
+plot(cars$speed,cars$dist)
+corr <- cor.test(x=cars$speed, y=cars$dist, method = 'spearman')
+corr
+
 
 heartatk = read.table("http://statland.org/R/RC/heartatk4R.txt",header=TRUE) 
 heartatk
@@ -29,11 +37,35 @@ hist(x, prob=TRUE, xlim=c(0, 150), ylim=c(0, 1), main='pdf of Chi-square (df = 1
 lines(density(x), col='orange')
 abline(v=147)
 
-#Fisher Test
-fisher.test(table(SEX,DIED))
+# FISHER EXACT TEST in R
+#
+# Example data:
+# Area     With rabies      Without rabies
+# ----     -----------      --------------
+#  Earl          10                5 
+#  Will           6                17
+# Read the 2x2 contingency table, r=2 rows (X values), c=2 columns (Y values):
+data<-scan()
+
+tab <- t(matrix(data, nrow=2,ncol=2))  # transpose, as usual
+tab         # view the table to make sure it is right
+
+
+fisher.test(tab)   # performs a test of independence for matrix input.
 
 #McNemar Test
-mcnemar.test(table(SEX,DIED))
+## Agresti (1990), p. 350.
+## Presidential Approval Ratings.
+##  Approval of the President's performance in office in two surveys,
+##  one month apart, for a random sample of 1600 voting-age Americans.
+Performance <-
+  matrix(c(794, 86, 150, 570),
+         nrow = 2,
+         dimnames = list("1st Survey" = c("Approve", "Disapprove"),
+                         "2nd Survey" = c("Approve", "Disapprove")))
+Performance
+mcnemar.test(Performance)
+## => significant change (in fact, drop) in approval ratings
 
 library(ISwR)
 attach(energy)
@@ -43,7 +75,6 @@ energy
 wilcox.test(expend~stature)
 
 #wilcox signed rank test
-#n is the number of pairs, nn is number of observations
 daily.intake<-c(5260,5470,5640,
                 6180, 6390,6515,6805,7515,7515,
                 8230,8770)
